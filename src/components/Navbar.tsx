f235c0cac12f0e897ebdafb51f5b24cadbade258
Brandon James Whitaker <whitak44@purdue.edu>
2025-04-22 16:57:46 -0400

    Update Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,15 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+// import { useSelector, useDispatch } from 'react-redux';
 
 const Navbar = () => {
 
+    // const isLogin = useSelector ((state: RootState) => state.auth.isLogin);
+    // const navigate = useNavigate();
+    // const dispatch = useDispatch();
+    // const handleClick = () => {
+    //     dispatch(logout());
+    //     navigate("/login");
+    // }
 
     return(
         <nav className = "navbar">
@@ -10,15 +18,31 @@ const Navbar = () => {
                     <Link to = "/">Home</Link>
                 </li>
                 <li>
-                    <Link to = "/Products">Products</Link>
+                    <Link to = "/products">Products</Link>
                 </li>
                 <li>
-                    <Link to = "/Upcoming">Upcoming</Link>
+                    <Link to = "/upcoming">Upcoming</Link>
+                </li>
+        {/* delete the below lines */}
+                <li>
+                    <Link to = "/register" >Register</Link>
                 </li>
                 <li>
-                    
+                    <Link to = "/login" >Login</Link>
                 </li>
+
+{/* delete the above lines later into production */}
+
             </ul>
+            {/* {
+                isLogin ?
+                <button onClick = {handleClick}>Logout</button>
+                :
+                <ul>
+                    <li><Link to = "/register" >Register</Link></li>
+                    <li><Link to = "/login" >Login</Link></li>
+                </ul>
+            } */}
         </nav>
     )
 }
