ffa3b9a244d73e1e74686f1816783f318dda7719
Brandon James Whitaker <whitak44@purdue.edu>
2025-04-22 16:55:42 -0400

    navbar update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,22 @@
-
+import { Link, useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
 
 
     return(
-        <nav classname = "navbar">
+        <nav className = "navbar">
             <ul>
                 <li>
                     <Link to = "/">Home</Link>
                 </li>
                 <li>
-                    <Link to = "/Products"
+                    <Link to = "/Products">Products</Link>
+                </li>
+                <li>
+                    <Link to = "/Upcoming">Upcoming</Link>
+                </li>
+                <li>
+                    
                 </li>
             </ul>
         </nav>
