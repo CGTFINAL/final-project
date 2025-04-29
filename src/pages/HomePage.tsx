b5f1b7b681a0b25147ab196be92b474544ca0578
xM4ddy <59680197+xM4ddy@users.noreply.github.com>
2025-04-29 16:36:46 -0400

    add quote to homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,6 +3,8 @@ const HomePage = () => {
         <>
             <h1>Welcome To React Attack!</h1>
             <img src="./reactLogo.svg" style={{ width: '100px', height: '100px' }}></img>
+            <p>"React Attack is the ultimate destination for mastering React development!"</p>
+            <p>- John React</p>
         </>
     )
 }
