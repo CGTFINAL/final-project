2617dfc6f54483148021932a354d356caf29e2ef
xM4ddy <59680197+xM4ddy@users.noreply.github.com>
2025-04-29 16:32:11 -0400

    Tiny Bit More on Homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,9 @@
 const HomePage = () => {
-    return(
-        <h1>Hi</h1>
+    return (
+        <>
+            <h1>Welcome To React Attack!</h1>
+            <img src="./reactLogo.svg" style={{ width: '100px', height: '100px' }}></img>
+        </>
     )
 }
 
