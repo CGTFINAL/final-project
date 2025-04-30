620f59d5fd25d5321b32ba8325d4643531be867c
samuelkroot <samuel.k.root@gmail.com>
2025-04-29 23:56:47 -0400

    Warn group of jank

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,3 +1,5 @@
+// when you use the cart from the context, you will need to check for whether size and color are an empty string or not to determine whether to render the accompanying elements
+
 const CartPage = () => {
     return (<></>);
 }
