c9695230d89b86e311a3901d26d429c0d82c8b7b
samuelkroot <samuel.k.root@gmail.com>
2025-04-30 00:10:09 -0400

    Add CartProvider to the app

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { AuthProvider } from './contexts/AuthContext.tsx';
 import './index.css';
 import App from './App.tsx';
+import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
+import { AuthProvider } from './contexts/AuthContext.tsx';
+import { CartProvider } from './contexts/CartContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <AuthProvider>
-            <App />
+            <CartProvider>
+                <App />
+            </CartProvider>
         </AuthProvider>
     </StrictMode>
 );
